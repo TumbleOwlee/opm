eed106859d2e9213a9a2f37a35b2a7f21fb7bc77
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-15 23:18:35 +0000

    * reduced vignette files under pkg/ to PDF files * made calls to suggested packages in tests and examples conditional
    git-svn-id: svn://r-forge.r-project.org/svnroot/opm/pkg/opm@393 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/test_multcomp.R
+++ inst/tests/test_multcomp.R
@@ -77,6 +77,9 @@ test_that("opm_mcp converts 'model' arguments", {
 })
 
 
+if (suppressWarnings(suppressPackageStartupMessages(
+  require(multcomp, quietly = TRUE, warn.conflicts = FALSE)))) {
+
 ## opm_mcp
 test_that("opm_mcp converts numeric 'linfct' arguments", {
   got <- opm_mcp(EXPL.DF, model = list("organism", "run"),
@@ -429,6 +432,9 @@ test_that("'Pairs' contrast type can be combined with non-syntactic names", {
 })
 
 
+}
+
+
 ################################################################################
 
 
@@ -439,99 +445,9 @@ test_that("'Pairs' contrast type can be combined with non-syntactic names", {
 ################################################################################
 
 
-if (FALSE) {
-# get example objects
-if (!exists("TEST.DIR"))
-  attach(objects_for_testing())
-# vorbereiten der datenobjekte
-EXPL.OPMS <- c(THIN.AGG, THIN.AGG)
-EXPL.DF <- extract(EXPL.OPMS, as.labels = list("organism", "run"),
-  subset = "A", dataframe = TRUE)
-
-# dataframe erzeugen um die dimensionen der contrast-matrix zu kennen
-EXPL. <- opm_mcp(EXPL.DF, model = ~ J(Well + run), linfct = c(Dunnett = 1),
-  output = "data")
-
-# funktion um contrastmatrix aufzuspannen:
-
-con.mat <- function(object, model) {
-
-  EXPL.1 <- opm_mcp(object, model = model, # hier muss markus was machen
-    linfct = c(Dunnett = 1), output = "data")
-
-
-
-  con <- matrix(c(1, -1), nrow = 1, ncol = 2, byrow = TRUE)
-  con.mat <- kronecker(diag(1, 96), con) # TODO: adapt number of wells
-
-  val.pos <- which(colnames(EXPL.1) == "Value")
-  ex.com <- colnames(EXPL.[, val.pos : c(dim(EXPL.)[2])])[2]
-
-  colnames(con.mat) <- levels(EXPL.[, ex.com])
-  # schleife um die rownames zusammenzusetzen
-  con.rn <- c()
-  for (i in seq(1, length(EXPL.[, ex.com]) / 2, by = 2)) {
-    con.rn[i] <- paste(levels(EXPL.[, ex.com])[i], " - ",
-      levels(EXPL.[, ex.com])[i + 1], sep = "")
-  }
-  # rownames anfügen
-  con.rn <- con.rn[seq(1, length(EXPL.[, ex.com]) / 2, by = 2)]
-  rownames(con.mat) <- con.rn
-  return(con.mat)
-}
-
-# erzeugen der kontrastmatrix
-test.con <- con.mat(EXPL.DF, model = ~ J(Well + run))
-
-# test rechnen
-EXPL.CON.MAT <- opm_mcp(EXPL.DF, model = ~ J(Well + run),
-  linfct = test.con)
-# output ist ganz normales glht-object (mit 96 vergleichen)
-
-################################################################################
-
-# check, if names-attributes is existing
-
 ## annotated
-test_that("OPM-object", {
-  # check class and dimension when testing with first plate from THIN.AGG
-  x <- annotated(THIN.AGG[1], "A")
-  expect_is(x, "list")
-  expect_equal(length(x), 1)
-  expect_is(x[[1]], "numeric")
-  expect_equal(length(x[[1]]), 96)
-  # check, if names-attributes is existing
-})
+## UNTESTED
 
-## annotated
-test_that("OPMS-object", {
-  # check class and dimension when testing with THIN.AGG
-  x <- annotated(THIN.AGG, "A")
-  expect_is(x, "list")
-  expect_equal(length(x), 2)
-  expect_is(x[[1]], "numeric")
-  expect_is(x[[2]], "numeric")
-  expect_equal(length(x[[1]]), 96)
-  expect_equal(length(x[[2]]), 96)
-})
-
-
-## annotated
-test_that("glht-object", {
-  # check class and dimension when testing with EXPL.CON.MAT
-  x <- annotated(EXPL.CON.MAT)
-  expect_equal(length(x), 96)
-  expect_is(x, "numeric")
-})
-
-
-#x <- annotated(THIN.AGG[1], "A", output = "numeric")
-
-
-#x <- annotated(THIN.AGG[1], "A", output = "logical")
-
-
-}
 
 
 
--- tests/run-all.R
+++ tests/run-all.R
@@ -14,7 +14,6 @@ library(opm)
 if (require(testthat) &&
     # 'testhat' does not work under Solaris (Ripley, pers. comm. 14-02-2012);
     # this is a blind-flight attempt to turn it off that seemed to work so far
-    #
-    !grepl("solaris", Sys.info()[["sysname"]], TRUE, TRUE)) {
+    !grepl("solaris", R.version$platform, TRUE, TRUE)) {
   test_package("opm")
 }
